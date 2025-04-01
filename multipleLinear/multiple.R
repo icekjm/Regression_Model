36ea6953d5cc8ab2a2ad580995494622573b2ae0
icekjm <icekjm1029@gmail.com>
2025-04-01 13:09:50 +0900

    fix:multiple linear regression

--- multipleLinear/multiple.R
+++ multipleLinear/multiple.R
@@ -67,8 +67,9 @@ summary(h4_lm)
 #실제 분석 사례
 chemical = read.csv("c:/data/reg/chemical.csv")
 head(chemical)
+#1번째 열 제외하고 데이터를 가져옴
+summary(chemical[,-1]) 
 
-summary(chemical[,-1]) #1번째 열 제외하고 데이터를 가져옴
 #첫 번째 열을 제외한 나머지 변수들 간의 상관계수 
 #+1 : 완벽한 양의 상관관계, 0:상관관계 없음, -1: 완력한 음의상관관계
 cor(chemical[,-1])
@@ -76,15 +77,25 @@ chemical_lm = lm(loss ~ speed + temp,  data=chemical)
 summary(chemical_lm)
 
 library(car)
+#특정 독립변수와 종속변수 사이의 순수한 관계
+#speed | others : loss에 순수하게 영향을 끼치는 speed
 avPlots(chemical_lm)
 
 anova(chemical_lm)
+#speed의 p-value 계산
+pf(45.02, df1 = 1, df2 = 9, lower.tail = FALSE)
+#temp의 p-value 계산
+pf(7.5867, df1 = 1, df2 = 9, lower.tail = FALSE)
 
+#독립변수인 speed와 잔차(residuals)의 관계를 시각화
 plot(chemical$speed, chemical_lm$resid)
+#그래프 상의 특정 점을 "마우스로 클릭해서 식별"
 identify(chemical$speed, chemical_lm$resid)
 plot(chemical$temp, chemical_lm$resid)
 identify(chemical$temp, chemical_lm$resid)
 
 plot(chemical_lm$fitted, chemical_lm$resid)
+# 그래프에 수평선 하나 추가하는 명령어
+# h = 0	Y축의 값이 0인 위치에 수평선을 그림
 abline(h=0, lty=2)
 identify(chemical_lm$fitted, chemical_lm$resid)
